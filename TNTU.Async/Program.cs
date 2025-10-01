62ecd5f17d83a85a25803f5cecff850095fec604
Mykola Zaiarnyi <mykola.zaiarnyi@eleks.com>
2025-10-01 09:29:58 +0300

    Lab2: Async await

---
+++ TNTU.Async/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Before DB call");
+Task<int> resultTask = MakeDbQuery();
+Console.WriteLine("DB call is made");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Iteration {i}");
+}
+
+int result = await resultTask;
+Console.WriteLine($"result: {result}");
+
+
+var resultTask1 = Task.Run(() =>
+{
+    Console.WriteLine($"INSIDE TASK RUN: Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine(42);
+    return 42;
+});
+
+Console.WriteLine($"OUTSIDE TASK RUN: Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+var result1 = await resultTask;
+Console.WriteLine($"OUTSIDE TASK RUN AFTER AWAIT: Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+Console.WriteLine(result);
+
+
+
+async Task<int> MakeDbQuery()
+{
+    await Task.Delay(3000);
+    Console.WriteLine("Making DB call...");
+    return 42;
+}
+
